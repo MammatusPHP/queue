37f7d137b5709428fc0b0be1afa7e63801f31fa9
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-10-19 01:04:56 +0200

    Jump go PSR-3 v3

--- src/Generated/Hydrator.php
+++ src/Generated/Hydrator.php
@@ -6,49 +6,62 @@ namespace Mammatus\Queue\Generated;
 
 use EventSauce\ObjectHydrator\IterableList;
 use EventSauce\ObjectHydrator\ObjectMapper;
+use EventSauce\ObjectHydrator\PropertySerializers\SerializeArrayItems;
+use EventSauce\ObjectHydrator\PropertySerializers\SerializeDateTime;
+use EventSauce\ObjectHydrator\PropertySerializers\SerializeUuidToString;
 use EventSauce\ObjectHydrator\UnableToHydrateObject;
 use EventSauce\ObjectHydrator\UnableToSerializeObject;
 use Generator;
+use LogicException;
+use Mammatus\Queue\BuildIn\EmptyMessage;
+use Throwable;
+
+use function assert;
+use function count;
 
 class Hydrator implements ObjectMapper
 {
     private array $hydrationStack = [];
-    public function __construct() {}
+
+    public function __construct()
+    {
+    }
 
     /**
-     * @template T of object
      * @param class-string<T> $className
+     *
      * @return T
+     *
+     * @template T of object
      */
     public function hydrateObject(string $className, array $payload): object
     {
-        return match($className) {
-            \Mammatus\Queue\BuildIn\EmptyMessage::class => $this->hydrateMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage($payload),
+        return match ($className) {
+            EmptyMessage::class => $this->hydrateMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage($payload),
             default => throw UnableToHydrateObject::noHydrationDefined($className, $this->hydrationStack),
         };
     }
-    
-            
-    private function hydrateMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage(array $payload): \Mammatus\Queue\BuildIn\EmptyMessage
+
+    private function hydrateMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage(array $payload): EmptyMessage
     {
-        $properties = []; 
+        $properties    = [];
         $missingFields = [];
         try {
-        } catch (\Throwable $exception) {
-            throw UnableToHydrateObject::dueToError(\Mammatus\Queue\BuildIn\EmptyMessage::class, $exception, stack: $this->hydrationStack);
+        } catch (Throwable $exception) {
+            throw UnableToHydrateObject::dueToError(EmptyMessage::class, $exception, stack: $this->hydrationStack);
         }
 
         if (count($missingFields) > 0) {
-            throw UnableToHydrateObject::dueToMissingFields(\Mammatus\Queue\BuildIn\EmptyMessage::class, $missingFields, stack: $this->hydrationStack);
+            throw UnableToHydrateObject::dueToMissingFields(EmptyMessage::class, $missingFields, stack: $this->hydrationStack);
         }
 
         try {
-            return new \Mammatus\Queue\BuildIn\EmptyMessage(...$properties);
-        } catch (\Throwable $exception) {
-            throw UnableToHydrateObject::dueToError(\Mammatus\Queue\BuildIn\EmptyMessage::class, $exception, stack: $this->hydrationStack);
+            return new EmptyMessage(...$properties);
+        } catch (Throwable $exception) {
+            throw UnableToHydrateObject::dueToError(EmptyMessage::class, $exception, stack: $this->hydrationStack);
         }
     }
-    
+
     private function serializeViaTypeMap(string $accessor, object $object, array $payloadToTypeMap): array
     {
         foreach ($payloadToTypeMap as $payloadType => [$valueType, $method]) {
@@ -57,7 +70,7 @@ class Hydrator implements ObjectMapper
             }
         }
 
-        throw new \LogicException('No type mapped for object of class: ' . $object::class);
+        throw new LogicException('No type mapped for object of class: ' . $object::class);
     }
 
     public function serializeObject(object $object): mixed
@@ -66,114 +79,99 @@ class Hydrator implements ObjectMapper
     }
 
     /**
-     * @template T
-     *
      * @param T               $object
      * @param class-string<T> $className
+     *
+     * @template T
      */
     public function serializeObjectOfType(object $object, string $className): mixed
     {
         try {
-            return match($className) {
+            return match ($className) {
                 'array' => $this->serializeValuearray($object),
-            'Ramsey\Uuid\UuidInterface' => $this->serializeValueRamsey⚡️Uuid⚡️UuidInterface($object),
-            'DateTime' => $this->serializeValueDateTime($object),
-            'DateTimeImmutable' => $this->serializeValueDateTimeImmutable($object),
-            'DateTimeInterface' => $this->serializeValueDateTimeInterface($object),
-            \Mammatus\Queue\BuildIn\EmptyMessage::class => $this->serializeObjectMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage($object),
-                default => throw new \LogicException("No serialization defined for $className"),
+                'Ramsey\Uuid\UuidInterface' => $this->serializeValueRamsey⚡️Uuid⚡️UuidInterface($object),
+                'DateTime' => $this->serializeValueDateTime($object),
+                'DateTimeImmutable' => $this->serializeValueDateTimeImmutable($object),
+                'DateTimeInterface' => $this->serializeValueDateTimeInterface($object),
+                EmptyMessage::class => $this->serializeObjectMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage($object),
+                default => throw new LogicException("No serialization defined for $className"),
             };
-        } catch (\Throwable $exception) {
+        } catch (Throwable $exception) {
             throw UnableToSerializeObject::dueToError($className, $exception);
         }
     }
-    
-    
+
     private function serializeValuearray(mixed $value): mixed
     {
         static $serializer;
-        
+
         if ($serializer === null) {
-            $serializer = new \EventSauce\ObjectHydrator\PropertySerializers\SerializeArrayItems(... [
-]);
+            $serializer = new SerializeArrayItems(...[]);
         }
-        
+
         return $serializer->serialize($value, $this);
     }
-
 
     private function serializeValueRamsey⚡️Uuid⚡️UuidInterface(mixed $value): mixed
     {
         static $serializer;
-        
+
         if ($serializer === null) {
-            $serializer = new \EventSauce\ObjectHydrator\PropertySerializers\SerializeUuidToString(... [
-]);
+            $serializer = new SerializeUuidToString(...[]);
         }
-        
+
         return $serializer->serialize($value, $this);
     }
-
 
     private function serializeValueDateTime(mixed $value): mixed
     {
         static $serializer;
-        
+
         if ($serializer === null) {
-            $serializer = new \EventSauce\ObjectHydrator\PropertySerializers\SerializeDateTime(... [
-]);
+            $serializer = new SerializeDateTime(...[]);
         }
-        
+
         return $serializer->serialize($value, $this);
     }
-
 
     private function serializeValueDateTimeImmutable(mixed $value): mixed
     {
         static $serializer;
-        
+
         if ($serializer === null) {
-            $serializer = new \EventSauce\ObjectHydrator\PropertySerializers\SerializeDateTime(... [
-]);
+            $serializer = new SerializeDateTime(...[]);
         }
-        
+
         return $serializer->serialize($value, $this);
     }
-
 
     private function serializeValueDateTimeInterface(mixed $value): mixed
     {
         static $serializer;
-        
+
         if ($serializer === null) {
-            $serializer = new \EventSauce\ObjectHydrator\PropertySerializers\SerializeDateTime(... [
-]);
+            $serializer = new SerializeDateTime(...[]);
         }
-        
+
         return $serializer->serialize($value, $this);
     }
 
-
     private function serializeObjectMammatus⚡️Queue⚡️BuildIn⚡️EmptyMessage(mixed $object): mixed
     {
-        \assert($object instanceof \Mammatus\Queue\BuildIn\EmptyMessage);
-        $result = [];
+        assert($object instanceof EmptyMessage);
 
-
-        return $result;
+        return [];
     }
-    
-    
 
     /**
-     * @template T
-     *
      * @param class-string<T> $className
      * @param iterable<array> $payloads;
      *
      * @return IterableList<T>
      *
      * @throws UnableToHydrateObject
+     *
+     * @template T
      */
     public function hydrateObjects(string $className, iterable $payloads): IterableList
     {
@@ -188,14 +186,14 @@ class Hydrator implements ObjectMapper
     }
 
     /**
-     * @template T
-     *
      * @param class-string<T> $className
      * @param iterable<array> $payloads;
      *
      * @return IterableList<T>
      *
      * @throws UnableToSerializeObject
+     *
+     * @template T
      */
     public function serializeObjects(iterable $payloads): IterableList
     {
